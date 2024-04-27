36e1927bf3237d0d4cf1996b637c8f6abd81db43
Dimas Zuda Fathul Akhir <65186413+dimaszuda@users.noreply.github.com>
2024-04-27 14:17:23 +0700

    update: corpus exclude sample second video

--- dashboard/corpus.py
+++ dashboard/corpus.py
@@ -4,7 +4,6 @@ IMAGE_TYPE = ['jpg', 'jpeg', 'png', 'gif', 'webp']
 FILE_TYPE = ['Image', 'Video']
 SAMPLE_DIR = '/Users/mac/Documents/DIMAS/yolov8/APD-Detection/APD-Detection-Yolov8/sample/'
 SAMPLE_VIDEO = {
-    'video 1': SAMPLE_DIR+ 'Construction People.mp4',
-    'video 2': SAMPLE_DIR+'video-contoh-trim.mov'
+    'video 1': SAMPLE_DIR+ 'Construction People.mp4'
 }
 CONFIDENCE_THRESHOLD = 0.5
