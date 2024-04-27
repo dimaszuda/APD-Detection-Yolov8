8c3c110750950f8ac25e70b3aadb5b633984603b
dimaszuda <dimas.zuda45@gmail.com>
2024-04-27 20:01:46 +0700

    update: model and sample path in corpus

--- dashboard/corpus.py
+++ dashboard/corpus.py
@@ -1,8 +1,8 @@
-MODEL_PATH = '/Users/mac/Documents/DIMAS/yolov8/APD-Detection/APD-Detection-Yolov8/model/best_1.pt'
+MODEL_PATH = '../model/best_1.pt'
 VIDEO_SOURCE = ['Sample', 'Local', 'Youtube']
 IMAGE_TYPE = ['jpg', 'jpeg', 'png', 'gif', 'webp']
 FILE_TYPE = ['Image', 'Video']
-SAMPLE_DIR = '/Users/mac/Documents/DIMAS/yolov8/APD-Detection/APD-Detection-Yolov8/sample/'
+SAMPLE_DIR = '../sample/'
 SAMPLE_VIDEO = {
     'video 1': SAMPLE_DIR+ 'Construction People.mp4'
 }
